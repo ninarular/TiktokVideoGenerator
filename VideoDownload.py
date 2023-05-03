d2210bf83a323b65e80989c37a26bd6e8d8eb36e
NotUnko <112636732+NotUnko@users.noreply.github.com>
2023-05-03 15:25:08 +0100

    Update VideoDownload.py

--- VideoDownload.py
+++ VideoDownload.py
@@ -13,7 +13,7 @@ def VideoDownload():
     # Set the output file name
     output_name = "File1V.mp4"
     # Set the output directory
-    output_dir = "/Users/nikhilnarula/Desktop/Tiktok"
+    output_dir = "output"
     # Create the output path by joining the output directory and file name
     output_path = os.path.join(output_dir, output_name)
     # Download the video
@@ -28,7 +28,7 @@ def AudioDownload():
     # Set the output file name
     output_name = "File1A.mp3"
     # Set the output directory
-    output_dir = "/Users/nikhilnarula/Desktop/Tiktok"
+    output_dir = "output"
     # Create the output path by joining the output directory and file name
     output_path = os.path.join(output_dir, output_name)
     # Download the audio
@@ -38,10 +38,10 @@ def AudioDownload():
 def Combine():
     import subprocess
     # Set the paths to the audio and video files
-    audio_path = "/Users/nikhilnarula/Desktop/Tiktok/File1A.mp3"
-    video_path = "/Users/nikhilnarula/Desktop/Tiktok/File1V.mp4"
+    audio_path = "output/File1A.mp3"
+    video_path = "output/File1V.mp4"
     # Set the output file path
-    output_path = "/Users/nikhilnarula/Desktop/Tiktok/Combined.mp4"
+    output_path = "output/Combined.mp4"
     # Use ffmpeg to combine the audio and video files
     subprocess.run(["ffmpeg", "-i", audio_path, "-i", video_path, "-c", "copy", output_path])
     print("Audio and video combined successfully!")
@@ -49,9 +49,9 @@ def Combine():
 
 def Spiltter():
     # Replace with the path to the YouTube video file
-    input_path = '/Users/nikhilnarula/Desktop/Tiktok/Combined.mp4'
+    input_path = 'output/Combined.mp4'
     # Replace with the path to the folder where you want to save the video clips
-    output_path = '/Users/nikhilnarula/Desktop/Tiktok'
+    output_path = 'output'
     # Split the video into 30 second clips
     os.system(f"ffmpeg -i {input_path} -c copy -map 0 -segment_time 30 -f segment {output_path}/%d.mp4")
 
@@ -66,5 +66,4 @@ VideoDownload()
 AudioDownload()
 Combine()
 Spiltter()
-
 
